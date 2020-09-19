d70b90525fcf5ea153e7cc4a31d6c9fc5abf99f4
tushar1999sharma <tushar1999sharma@gmail.com>
2020-09-19 18:09:47 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "Top-Tidings/precache-manifest.4e1a2bd73b0d487e52ad568dcfcdd00d.js"
+  "/Top-Tidings%20/precache-manifest.9e6bff52718118a61b0cc0141636e3c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("Top-Tidings/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Top-Tidings%20/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
